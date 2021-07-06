13fb4f7c1d30236aa05cd3179bd8bdbf0d639d18
Sergi Bonafe <sergib1998@gmail.com>
2021-07-06 02:15:24 +0200

    Styled and organized web

---
+++ web/index.js
@@ -0,0 +1,68 @@
+
+function fetchData() {
+    fetch('https://apfp-js-api.p0rtl.repl.co/ReqAll', { cache: "no-store" })
+        .then((res) => res.json())
+        .then((data) => {
+            let output = ""
+            const datArray = Object.keys(data);
+            datArray.forEach((datArray, index) => {
+                output += "<div class='ResponseWrapper'>"
+                output += `<p class="userId">${datArray}</p><div class='statusBar' id=${datArray}></div>`
+                for (var property in data[datArray]) {
+                    //console.log(datArray + property + data[datArray][property]);
+                    output += `<img class="rqIMG" data-user-id="${datArray}" src='${data[datArray][property]}'></img>`
+                }
+                output += "</div>"
+            });
+
+            document.getElementById('myData').innerHTML = output;
+        })
+        .catch((error) => {
+            console.log(`Error Fetching data : ${error}`)
+            document.getElementById('myData').innerHTML = 'Error Loading Data'
+        })
+}
+
+function getApproved() {
+    appr = Array.from(document.getElementsByClassName('rqIMG'))
+    appr.forEach((appr, index) => {
+        src = appr.getAttribute("src")
+        usrId = appr.getAttribute("data-user-id")
+        svg = document.getElementById(usrId)
+        switch (src) {
+            case "0":
+                getPreviousSiblings(appr, "0");
+                svg.style.fill = "var(--pending)";
+                break;
+            case "1":
+                getPreviousSiblings(appr, "1");
+                svg.style.fill = "var(--accepted)";
+                break;
+            case "2":
+                getPreviousSiblings(appr, "2");
+                svg.style.fill = "var(--denied)";
+                break;
+        }
+    });
+};
+
+function getPreviousSiblings(el, val) {
+    var siblings = [];
+    while (el = el.previousSibling) { siblings.push(el); }
+    form = siblings[2];
+    form.setAttribute("value", val);
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    pageLoader();
+});
+function pageLoader() {
+    setTimeout(showSite, 1500);
+}
+function showSite() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myData").style.display = "grid";
+    getApproved();
+}
+
+fetchData();
