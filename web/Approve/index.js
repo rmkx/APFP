3792ce7e14336524de74c4035572e4a21921988e
Sergi Bonafe <sergib1998@gmail.com>
2021-07-06 02:41:54 +0200

    Reorganized website

--- web/Approve/index.js
+++ web/Approve/index.js
@@ -1,20 +1,20 @@
 
 function fetchData() {
-    fetch('https://apfp-js-api.p0rtl.repl.co/ReqAll', { cache: "no-store" })
+    fetch('https://apfp-js-api.p0rtl.repl.co/ReqAll')
         .then((res) => res.json())
         .then((data) => {
             let output = ""
             const datArray = Object.keys(data);
             datArray.forEach((datArray, index) => {
                 output += "<div class='ResponseWrapper'>"
-                output += `<p class="userId">${datArray}</p><div class='statusBar' id=${datArray}></div>`
+                output += `<p class="userId">${datArray}</p><div onclick="Approve(this)" class='statusBar' id=${datArray}></div>`
+                output += `<input type="text" class="ApprForm" name="${datArray}" value="0">`
                 for (var property in data[datArray]) {
                     //console.log(datArray + property + data[datArray][property]);
                     output += `<img class="rqIMG" data-user-id="${datArray}" src='${data[datArray][property]}'></img>`
                 }
                 output += "</div>"
             });
-
             document.getElementById('myData').innerHTML = output;
         })
         .catch((error) => {
@@ -51,6 +51,43 @@ function getPreviousSiblings(el, val) {
     while (el = el.previousSibling) { siblings.push(el); }
     form = siblings[2];
     form.setAttribute("value", val);
+}
+function getPreviousSiblings(el, val) {
+    var siblings = [];
+    while (el = el.previousSibling) { siblings.push(el); }
+    form = siblings[2];
+    form.setAttribute("value", val);
+}
+
+function getNextSiblings(el, val) {
+    var siblings = [];
+    while (el = el.nextSibling) { siblings.push(el); }
+    form = siblings[1];
+    form.setAttribute("value", val);
+}
+function Approve(elem) {
+    //console.log(elem)
+    var content = document.getElementById(elem.id);
+    var contentCss = getComputedStyle(content);
+    var contentFill = contentCss.backgroundColor;
+    //console.log(contentFill);
+    content.style.backgroundColor = "white";
+    switch (contentFill) {
+        case "rgb(233, 196, 106)":
+            getNextSiblings(content, "1");
+            content.style.backgroundColor = "var(--accepted)";
+            break;
+        case "rgb(89, 128, 77)":
+            getNextSiblings(content, "2");
+            content.style.backgroundColor = "var(--denied)";
+            break;
+        case "rgb(231, 111, 81)":
+            getNextSiblings(content, "0");
+            content.style.backgroundColor = "var(--pending)";
+            break;
+        default:
+            alert("Err: no status change match");
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
