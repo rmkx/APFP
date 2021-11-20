934c96f7ea1e40cd0e5b63a1c46003186c93ee24
p0rtL6 <80869244+p0rtL6@users.noreply.github.com>
2021-11-20 02:03:04 -0500

    Update build.js

--- build.js
+++ build.js
@@ -1,1 +1,78 @@
-// Fix
+const core = require("@actions/core");
+const cleanCSS = require("clean-css");
+const { writeFileSync } = require("fs");
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGODB_URI);
+
+async function read(query, type = "uid") {
+    await client.connect();
+
+    const res = await client
+        .db("apfp")
+        .collection("apfp")
+        .find(query ? { [type]: query } : {})
+        .toArray();
+
+    await client.close();
+
+    return res.length > 1 ? res : res[0];
+}
+
+function emplace(map, item, id) {
+    const itemVal = map.get(item);
+    if (!itemVal) map.set(item, [id]);
+    else itemVal.push(id);
+}
+
+async function compile() {
+    const data = await read();
+    writeFileSync("./dist/apfp.json", JSON.stringify(data));
+
+    const createRule = (uids, rules) =>
+        `${uids
+            .map(uid => `.root-3QyAh1[apfp-user-id="${uid}"]`)
+            .join()}{${rules.join("")}}`;
+
+    const backgrounds = new Map(
+        Object.entries({ animated: new Map(), static: new Map() })
+    );
+
+    for (const { img, aimg, uid } of data) {
+        emplace(backgrounds.get("static"), img, uid);
+        emplace(backgrounds.get("animated"), aimg, uid);
+    }
+
+    const raw = [...backgrounds]
+        .map(([type, map]) => {
+            return [...map]
+                .map(([img, uids]) => {
+                    return createRule(
+                        uids,
+                        type === "static"
+                            ? [
+                                  `--user-avatar-static:url("${img}");`,
+                                  "--user-avatar-opacity:0;",
+                              ]
+                            : [`--user-avatar-playing:url("${img}");`]
+                    );
+                })
+                .join("");
+        })
+        .join("");
+
+    const output = new cleanCSS({
+        level: {
+            2: {
+                all: true,
+            },
+        },
+    }).minify(raw);
+
+    return output.styles;
+}
+
+try {
+    compile().then(css => writeFileSync("./dist/usrbg.css", css));
+} catch (err) {
+    core.setFailed(err.message);
+}
